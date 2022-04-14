425d3eb8697c130a162466bea1ef447378cfe466
NiGaN <yusniel.abreu@gmail.com>
2022-04-14 18:01:56 -0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,46 @@
+print("hello world!")
+
+#---------------------------------------------------
+print("Day 1 - Python print function")
+
+print("the function is declared like this:")
+
+print("print('what to print')")
+
+#---------------------------------------------------
+
+print("hello"+" "+"angela")
+
+#--------------------------------------------------
+
+print("day 1 - strign manipulation")
+print("string concatenation is done  with the '+' sign")
+print("e.g print('hello ' + 'world')")
+print("new lines  can be  created  with  a backslash and n.")
+
+#-------------------------------------------------------
+
+print("hello "+ input("what is your name?")+"!")
+
+#----------------------------------------------------------
+print(len(input("insert your name ")))
+
+#--------------------------------------------------------
+
+a=input("size of a:")
+b=input("size of b: ")
+
+print("a:"+a+" b: "+b)
+
+c=a
+a=b
+b=c
+
+print("a:"+a+" b: "+b)
+
+#-------------------------------------------------------------
+
+city=input("what's your city name?\n")
+pet=input("what's your pet name?\n")
+
+print("band name: "+city+" "+pet)
